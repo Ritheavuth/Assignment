c8452dc3533d83f8780f5cd630cdd6e1d7abba24
Just S1 <hinchanritheavuth20@kit.edu.kh>
2022-01-13 22:41:26 +0700

    k

--- public/script.js
+++ public/script.js
@@ -1,43 +1,52 @@
-loadData()
-
-function loadData(){
-    fetch("http://localhost:3000/CountryInformation").then(
-        res =>{
-            res.json().then(
-                data=>{
-                    console.log(data)
-                    if(data.length > 0){
-                        var temp = "";
-                        data.forEach((d) => {
-                            temp += "<tr>";
-                            temp += "<td>" + d.id + "</td>";
-                            temp += "<td>" + d.country_name + "</td>";;
-                            temp += "<td>" + d.language + "</td>";;
-                            temp += "<td>" + d.region + "</td></tr>";;
-                        })
-
-                        document.getElementById("table").innerHTML = temp;
-                    }
-                }
-            )
-        }
-    )
-}
-
-var formdata = {
-    id : document.getElementById("id").value,
-    country_name : document.getElementById('country_name').value,
-    language : document.getElementById('language').value,
-    region : document.getElementById("region").value
-}
-
 function addRecord() {
-    var data = formdata
+    var formdata = {
+        id : document.getElementById("id").value,
+        country_name : document.getElementById('country_name').value,
+        language : document.getElementById('language').value,
+        region : document.getElementById("region").value
+    }
     fetch ("http://localhost:3000/CountryInformation", {
         method : "POST",
         headers : {"content-Type":"application/json"},
         body : JSON.stringify(formdata)
     }).then((res) => {
         res.json()
+    })
+}
+
+function deleteRecord() {
+    var formdata = {
+        id : document.getElementById("id").value,
+        country_name : document.getElementById('country_name').value,
+        language : document.getElementById('language').value,
+        region : document.getElementById("region").value
+    }
+    if (formdata.id > 0) {
+        fetch ("http://localhost:3000/CountryInformation/" + formdata.id, {
+        method : "Delete",
+        headers : {"content-Type":"application/json"},
+    })
+    }else {
+        fetch ("http://localhost:3000/CountryInformation" , {
+            method: "Delete",
+
+        })
+    }
+    
+}
+
+function display_in_table() {
+    fetch("http://localhost:3000/CountryInformation", {
+        method : "GET",
+    }).then((data) => {
+        data.array.forEach(element => {
+            row = 
+            `<tr>  
+                <td>Country_ID</td>
+                <td>Country_Name</td>
+                <td>Language</td>
+                <td>Region</td>
+            </tr>`
+        });
     })
 }
